9ad768df4159ef78b3dbf9038f0f427b7827bbdd
Gedeon <fobadeffo@gmail.com>
2025-09-20 04:29:01 +0100

    cloudinary appearence

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { register, login, getMe } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 const upload = require('../middlewares/uploadMiddleware');
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const cloudinary = require("../cloudinaryConfig");
 
 const router = express.Router();
 
@@ -9,13 +11,22 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 
-router.post("/upload-image", upload.single("image"), (req, res) => {
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "jobPortal",
+        allowed_formats: ["jpg", "jpeg", "png", "pdf", "xlm", "gif", "webp"],
+    },
+});
+
+const parser = multer({ storage });
+
+router.post("/upload-image", protect, parser.single("image"), async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: "No file uploaded" });
     }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-        req.file.filename
-    }`;
+    const imageUrl = req.file.path;
+
     res.status(200).json({ imageUrl });
 });
 
